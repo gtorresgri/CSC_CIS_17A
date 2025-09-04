2ecb6fdd26907557c02ab9e64ebd4bbdcc6dcdf8
gtorresgri <gtorresgri@student.rccd.edu>
2025-09-03 22:46:16 -0700

    hello world

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: Gunnar Torres
+ * Created on September 3, 2025, 6:00pm
+ * Purpose:  Hello World 
+ */
+
+// Libraries
+#include <iostream> 
+using namespace std;
+
+// Global Constants ONLY
+
+// Function Prototypes
+
+int main(int argc, char** argv) {
+    // Variable Declarations & Initializations
+    
+    cout << "Hello World" << endl;
+
+    return 0;
+}
